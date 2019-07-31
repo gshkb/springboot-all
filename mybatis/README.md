b3fe4093fff908d1c9e69a57ed9e1c7a7e356e81
何凯博 <w99699w@126.com>
2019-07-31 17:41:47 +0800

    mybatis整合单数据源

---
+++ mybatis/README.md
@@ -0,0 +1,4 @@
+##mybatis+durid+mysql的数据源配置
+## 附送常见的mybatis面试题:
+* ###请点击:http://www.mybatis.cn/category/interview/
+
--- mybatis/src/main/java/cn/gshkb/mybatis/MybatisApplication.java
+++ mybatis/src/main/java/cn/gshkb/mybatis/MybatisApplication.java
@@ -1,9 +1,12 @@
 package cn.gshkb.mybatis;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.*;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
 
 @SpringBootApplication
+@MapperScan("cn.gshkb.*.mapper")
 public class MybatisApplication {
 
 	public static void main(String[] args) {
