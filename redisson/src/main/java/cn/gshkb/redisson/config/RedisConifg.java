ddc1b0a4050575b52fed36433a3dd505778ef7f6
何凯博 <w99699w@126.com>
2019-09-20 14:18:53 +0800

    1.redisson 分布式锁案例

--- redisson/src/main/java/cn/gshkb/redisson/config/RedisConifg.java
+++ redisson/src/main/java/cn/gshkb/redisson/config/RedisConifg.java
@@ -7,6 +7,7 @@ import org.redisson.api.RedissonRxClient;
 import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 /**
  * 配置文件
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.Configuration;
  * @create 2019-09-07 17:23 v1.0
  **/
 @Configuration
+@Component
 public class RedisConifg {
 
 	@Bean
