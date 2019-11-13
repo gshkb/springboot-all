62c292f96454641d4303b8ea1960e03b22d0df77
何凯博 <w99699w@126.com>
2019-11-13 18:25:39 +0800

    1.jdk动态代理.模式1

--- designpattern/src/main/java/cn/gshkb/designpattern/proxy/jdkproxy/MyInvokerHandler.java
+++ designpattern/src/main/java/cn/gshkb/designpattern/proxy/jdkproxy/MyInvokerHandler.java
@@ -14,13 +14,20 @@ public class MyInvokerHandler implements InvocationHandler {
 
 	private Object people;
 
-	public MyInvokerHandler(PeopleInterface people){
-		this.people = people;
+	public MyInvokerHandler(PeopleInterface peopleInterface) {
+		this.people = peopleInterface;
 	}
+
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		Object invoke = method.invoke(people, args);
-		System.out.println("invoke:"+invoke.toString());
+		//调用目标方法
+		Object invoke = null;
+		try {
+			invoke = method.invoke(people, args);
+			System.out.println("invoke:" + invoke.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return invoke;
 	}
 }
--- designpattern/src/main/java/cn/gshkb/designpattern/proxy/jdkproxy/TestJdkProxy.java
+++ designpattern/src/main/java/cn/gshkb/designpattern/proxy/jdkproxy/TestJdkProxy.java
@@ -1,5 +1,7 @@
 package cn.gshkb.designpattern.proxy.jdkproxy;
 
+import java.lang.reflect.Proxy;
+
 /**
  * 测试类
  *
@@ -7,9 +9,12 @@ package cn.gshkb.designpattern.proxy.jdkproxy;
  * @create 2019-11-13 17:54 v1.0
  **/
 public class TestJdkProxy {
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws NoSuchMethodException {
+		ProxyTarget proxyTarget = new ProxyTarget();
+		MyInvokerHandler myInvokerHandler = new MyInvokerHandler(proxyTarget);
+		Object o = Proxy.newProxyInstance(proxyTarget.getClass().getClassLoader(), proxyTarget.getClass().getInterfaces(), myInvokerHandler);
 
 
 	}
+
 }
