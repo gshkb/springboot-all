e07b4bb52a170483cf8e7d944b222343b9aefe81
何凯博 <hekb@xiaoqiedu.cn>
2020-04-09 23:35:22 +0800

    1.新增sentinel nacos数据源

--- designpattern/src/main/java/cn/gshkb/designpattern/adapter/AdapterTest.java
+++ designpattern/src/main/java/cn/gshkb/designpattern/adapter/AdapterTest.java
@@ -1,12 +1,13 @@
 package cn.gshkb.designpattern.adapter;
 
-import cn.gshkb.designpattern.adapter.ITarget;
 import cn.gshkb.designpattern.adapter.classmode.ClassAdapter;
+import cn.gshkb.designpattern.adapter.objmode.ObjAdapter;
 
 public class AdapterTest {
 
     public static void main(String[] args) {
         testClassAdpter();
+        testObjAdpter();
     }
 
     /**
@@ -15,5 +16,10 @@ public class AdapterTest {
     private static void testClassAdpter() {
         ITarget target = new ClassAdapter();
         target.doing();
+    }
+
+    private static void testObjAdpter() {
+        ITarget adapter = new ObjAdapter(new Adaptee());
+        adapter.doing();
     }
 }
--- sentinel-server1-embedded/src/main/java/cn/gshkb/sentinelserver1embedded/SentinelServer1EmbeddedApplication.java
+++ sentinel-server1-embedded/src/main/java/cn/gshkb/sentinelserver1embedded/SentinelServer1EmbeddedApplication.java
@@ -1,13 +1,13 @@
-package cn.gshkb.sentinelserver1;
+package cn.gshkb.sentinelserver1embedded;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SentinelServer1Application {
+public class SentinelServer1EmbeddedApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SentinelServer1Application.class, args);
+        SpringApplication.run(SentinelServer1EmbeddedApplication.class, args);
     }
 
 }
