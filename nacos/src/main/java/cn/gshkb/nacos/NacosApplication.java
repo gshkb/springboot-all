c91906a10606473b09657b3de739290788d8d723
hekb <w99699w@126.com>
2020-04-09 15:10:54 +0800

    1.添加nacos

--- nacos/src/main/java/cn/gshkb/nacos/NacosApplication.java
+++ nacos/src/main/java/cn/gshkb/nacos/NacosApplication.java
@@ -1,9 +1,11 @@
 package cn.gshkb.nacos;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@NacosPropertySource(dataId = "cn.gshkb.nacos.config",autoRefreshed = true)
 public class NacosApplication {
 
     public static void main(String[] args) {
