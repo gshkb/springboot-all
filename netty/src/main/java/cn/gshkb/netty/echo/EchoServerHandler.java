d28bcc85b6ecdbef5f55204a6c8492e2a1be9eb6
hekb <w99699w@126.com>
2020-04-29 09:30:11 +0800

    1.添加io

---
+++ netty/src/main/java/cn/gshkb/netty/echo/EchoServerHandler.java
@@ -0,0 +1,23 @@
+package cn.gshkb.netty.echo;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
